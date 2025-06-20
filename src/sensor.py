23ca9ad2846e6290ed38b5859bb03a5b40b21896
Marcos  Castilho <marcosalmaa@icloud.com>
2025-06-20 20:35:50 +0800

    Added sensors and display constructors

--- src/sensor.py
+++ src/sensor.py
@@ -1,2 +1,22 @@
 class Sensor:
-    pass
+
+    def __init__(self,
+                 id,
+                 car_park,
+                 is_active = False):
+        self.id = id
+        self.car_park = car_park
+        self.is_active = is_active
+
+
+        def __str__(self):
+            return f"Sensor {self.id} - {car_park} is {'active' if self.is_active else 'inactive'}."
+
+
+
+class EntrySensor(Sensor):
+    ...
+
+
+class ExitSensor(Sensor):
+        ...
